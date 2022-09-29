0dd765c444f2879a66fc8f66589a065075c2042f
Kamet Güler <kametguler@gmail.com>
2022-09-30 02:50:35 +0300

    fixing

--- blog2022/settings/production.py
+++ blog2022/settings/production.py
@@ -1,4 +1,5 @@
 from .base import MIDDLEWARE, BASE_DIR
+import dj_database_url
 import os
 
 DEBUG = False
@@ -9,6 +10,12 @@ SECRET_KEY = 'django-insecure-#sfr+%sg#mxffpue$m-3q(c-a1bakqh^ko!qglc0q@rn31qe)p
 
 META_SITE_PROTOCOL = 'https'
 META_SITE_DOMAIN = "petinven.herokuapp.com"
+
+db_from_env = dj_database_url.config(conn_max_age=600)
+DATABASES = {
+    'default': {}
+}
+DATABASES['default'].update(db_from_env)
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
