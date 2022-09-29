debd773b8134b9474c515d7fb24fe6f409661675
Kamet Güler <kametguler@gmail.com>
2022-09-30 02:36:54 +0300

    production configuration

--- blog2022/settings/production.py
+++ blog2022/settings/production.py
@@ -1,8 +1,15 @@
+from .base import MIDDLEWARE, BASE_DIR
+import os
+
 DEBUG = False
 
-ALLOWED_HOSTS = ["localhost"]
+ALLOWED_HOSTS = ["petinven.herokuapp.com"]
 
 SECRET_KEY = 'django-insecure-#sfr+%sg#mxffpue$m-3q(c-a1bakqh^ko!qglc0q@rn31qe)p'
 
 META_SITE_PROTOCOL = 'https'
 META_SITE_DOMAIN = "petinven.com"
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+MIDDLEWARE.insert(1, 'whitenoise.middleware.WhiteNoiseMiddleware')
