c0bdab28db359b53aec2e346cfcdac306b4aab8a
Kamet Güler <kametguler@gmail.com>
2022-09-30 12:53:51 +0300

    aws settings

--- blog2022/settings/production.py
+++ blog2022/settings/production.py
@@ -1,4 +1,6 @@
-from .base import MIDDLEWARE, BASE_DIR
+import storages.backends.s3boto3
+
+from .base import MIDDLEWARE, BASE_DIR, INSTALLED_APPS
 import dj_database_url
 import os
 
@@ -8,8 +10,19 @@ ALLOWED_HOSTS = ["petinven.herokuapp.com"]
 
 SECRET_KEY = 'django-insecure-#sfr+%sg#mxffpue$m-3q(c-a1bakqh^ko!qglc0q@rn31qe)p'
 
+INSTALLED_APPS.append('storages')
+
 META_SITE_PROTOCOL = 'https'
 META_SITE_DOMAIN = "petinven.herokuapp.com"
+
+# AWS SETTINGS
+AWS_ACCESS_KEY_ID = 'AKIAQKSY4ZHKB2CAY647'
+AWS_SECRET_ACCESS_KEY = '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'
+AWS_STORAGE_BUCKET_NAME = 'petinven-blog'
+
+AWS_S3_FILE_OVERWRITE = False
+AWS_DEFAULT_ACL = None
+DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
 db_from_env = dj_database_url.config(conn_max_age=600)
 DATABASES = {
