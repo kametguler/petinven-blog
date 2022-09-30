1fc2be539871579ed4958cabbbfa4a8a6afce7cc
Kamet Güler <kametguler@gmail.com>
2022-09-30 23:31:23 +0300

    404 page

--- blog2022/helpers.py
+++ blog2022/helpers.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
+from blog.sidebar_queries import sidebar_query
 
 
 def not_found(request, exception):
-    return render(request, 'not-found.html')
+    return render(request, 'not-found.html', sidebar_query())
