e4ec967f40a3df1fd6d08bab6f13dff056fd5d69
Kamet Güler <kametguler@gmail.com>
2022-09-30 23:28:20 +0300

    404 page

--- blog2022/urls.py
+++ blog2022/urls.py
@@ -29,3 +29,5 @@ urlpatterns = [
     path('', include('blog.urls')),
     path('sitemap.xml', sitemap, {'sitemaps': sitemaps})
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+handler404="blog2022.helpers.not_found"
